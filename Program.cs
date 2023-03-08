6264556bc9790327fe0b10e357afed8713a4ba71
sipra <sipramishra085@gmail.com>
2023-03-08 10:20:56 +0530

    first file

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FirstProgram1
+{
+    class Program
+
+    {
+        public static void Main(string[] args)
+        {
+            //int a = 10;
+            // int b = 20;
+            // double num = 2.5;
+            // char ch = 'a';
+            // bool val = true;
+            // string str = "Bridgelabz";
+
+
+            // Console.WriteLine("The integer value is" + a + b);
+            // Console.WriteLine("The double  value is" + num);
+            // Console.WriteLine("The integer value is" + ch);
+            //Console.WriteLine("The integer value is" + val);
+            // Console.WriteLine("The integer value is" + str);
+
+            //Console.WriteLine("please enter the username");
+           // string username = Console.ReadLine();
+
+           // int val = username.Length;
+
+           // Console.WriteLine("the length" + val);
+            //if (username.Length>3)
+
+            //{
+               // Console.WriteLine("hello" + username + " , How are you");
+               // Console.WriteLine("hello {0} {1} {2},How are you", username, "sipra", "ggtyg");
+            //}
+           // else
+            //{
+             //   Console.WriteLine("invalid username");
+         //   }
+        // Console.WriteLine("Enter Year");
+          //  int year = 2023;
+            //if (year % 400 == 0) 
+            //{
+              //  Console.WriteLine(year+"is a leap year");
+           // }
+            //else
+            //{
+              //  Console.WriteLine(year+"is not a leap year");
+            //}
+            //Console.ReadLine();
+            flipcoin.flipthecoin();
+        }
+   }
+}
+
+
